fa92b2560aa7829223a000fe74cc52933387a9b5
Samyak Shah <samyakshah3008@gmail.com>
2021-09-09 18:02:20 +0530

    REFACTORED

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
         
         if (parseInt(cashGiven.value) >= parseInt(billAmount.value)) { 
             const amountToBeReturned = cashGiven.value - billAmount.value; 
-            console.log(amountToBeReturned)
+        
             notesSection.style.display = 'block'
             calculateChange(amountToBeReturned);
         }
